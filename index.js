dedacbc1edf2816563492ae5bca8da9c5b570f00
sivasai04 <88765998+sivasai04@users.noreply.github.com>
2023-08-12 19:48:52 +0530

    Update index.js

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.use(cors());
 
 app.use('/posts', postRoutes);
 
-const CONNECTION_URL = 'mongodb+srv://sivasaikrishna:@Si#va$2002/@cluster0.liu3ggk.mongodb.net/?retryWrites=true&w=majority';
+const CONNECTION_URL = 'mongodb+srv://sivasai:siva69261@cluster0.liu3ggk.mongodb.net/?retryWrites=true&w=majority';
 const PORT = process.env.PORT|| 5000;
 
 mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
